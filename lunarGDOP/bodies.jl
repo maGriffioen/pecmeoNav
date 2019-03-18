f82bdff20199ae171c95316d319bd2bb899b2361
Mischa <m.a.griffioen@student.tudelft.nl>
2019-03-18 15:00:59 +0100

    Updated body information

--- lunarGDOP/bodies.jl
+++ lunarGDOP/bodies.jl
@@ -2,8 +2,29 @@
 #Replace with well-sourced numbers
 
 struct Body
+    name::String
     gravitationalParameter::Float64
     radius::Float64
 end
 
-earth = Body(3.986004418e14, 6371e3)
+Body(; name = "", gravitationalParameter, radius) =
+    Body(name, gravitationalParameter, radius)
+
+earth = Body(
+    name = "Earth",
+    gravitationalParameter =3.986004418e14,
+    radius = 6371e3
+)
+
+function bodyPosition(bodyName::String, time::Number)
+    if (bodyName == "Earth")
+        pos3d = (0.0, 0.0, 0.0)
+    elseif (bodyName == "Moon")
+        pos3d = (384e6, 0.0, 0.0)
+    else
+        error("Body is unknown")
+    end
+    return pos3d
+
+end
+bodyPosition(body::Body, time::Number) = bodyPosition(body.name, time)
