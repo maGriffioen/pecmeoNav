967004a3b087910ae24320cf0d2d436e9ac84b08
Mischa <m.a.griffioen@student.tudelft.nl>
2019-03-19 15:00:23 +0100

    Change static Moon location to Lunar orbit

--- lunarGDOP/bodies.jl
+++ lunarGDOP/bodies.jl
@@ -15,12 +15,18 @@ earth = Body(
     gravitationalParameter =3.986004418e14,
     radius = 6371e3
 )
+moon = Body(
+    name = "Moon",
+    gravitationalParameter = 4.9048695e12,
+    radius = 1737e3
+)
+lunarOrbit = KeplerOrbit(384e6, 0.0, deg2rad(23.4 + 5.14), 0.0, 0.0, 0.0, earth)
 
 function bodyPosition(bodyName::String, time::Number)
     if (bodyName == "Earth")
         pos3d = (0.0, 0.0, 0.0)
     elseif (bodyName == "Moon")
-        pos3d = (384e6, 0.0, 0.0)
+        pos3d = position(propagateKeplerOrbit(lunarOrbit, time))
     else
         error("Body is unknown")
     end
