47a3ef7b5ffc9238cfc3c5cff7cd694a717a1c14
Mischa <m.a.griffioen@student.tudelft.nl>
2019-03-19 15:00:04 +0100

    Added exception handling for singular DOP matrix

--- lunarGDOP/navGeom.jl
+++ lunarGDOP/navGeom.jl
@@ -15,7 +15,6 @@ function findNavCovmat(
         r  = norm(posdiff)             #Total distance
         mat_A[isat, 1:3] = posdiff ./ r             #Write unit vector distances
     end
-
     # Return matrix of covariances
     return inv(mat_A' * mat_A)
 end
@@ -23,16 +22,22 @@ findNavCovmat( refPoint::Tuple{Number, Number, Number},
      constel::KeplerConstellation ) = findNavCovmat(refPoint, position(constel))
 
 #Find the GDOP for reference point and constellation
+#Uses Earth position at t=0 and Earth radius for shadowing
 function findNavGDOP(
     refPoint::Tuple{Number, Number, Number},
     constelData::Array{Tuple{Float64, Float64, Float64}, 1} )
+    los = hasLineOfSight(refPoint, constelData, bodyPosition(earth, 0), earth.radius)
+    GDOP = 1e9   #If gdop cannot be calculated (too little satellites or bad geometry)
+    try
+        mat_Q = findNavCovmat(refPoint, constelData[los])
+        GDOP = sqrt(sum(diag(mat_Q)))   #Calculate gdop
+    catch
+    end
 
-    mat_Q = findNavCovmat(refPoint, constelData)
-    return sqrt(sum(diag(mat_Q)))     #GDOP
+    return GDOP
 end
 findNavGDOP(refPoint::Tuple{Number, Number, Number},
     constel::KeplerConstellation) = findNavGDOP(refPoint, position(constel))
-
 
 function hasLineOfSight(receiverLocation::Tuple{Float64, Float64, Float64},
     transmitterLocation::Tuple{Float64, Float64, Float64},
@@ -63,3 +68,6 @@ hasLineOfSight(receiverLoc::Tuple{Float64, Float64, Float64},
     bodyRadius::Float64) =
     [hasLineOfSight(receiverLoc, transmitterLoc[satid], bodyLoc, bodyRadius)
         for satid in 1:size(transmitterLoc, 1)]
+
+hasLineOfSightEarth(receiverLoc, transmitterLoc) =
+    hasLineOfSight(receiverLoc, transmitterLoc, bodyPosition(earth.name, 0), earth.radius)
