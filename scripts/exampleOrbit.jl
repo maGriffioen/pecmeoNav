487515b2120653683d02a65688bdbc4c2e06e9ce
Mischa <m.a.griffioen@student.tudelft.nl>
2019-04-03 18:11:42 +0200

    Added shadowing to signal generation. Improved kinematic estimation.
    Kinematic design matrix is now built per-measurement. Added phase bias per navsat arch. added dynamic expansion of estimation vector, with aprior from pseudorange-phase.

--- scripts/exampleOrbit.jl
+++ scripts/exampleOrbit.jl
@@ -2,82 +2,91 @@ using Plots, ProgressMeter, LinearAlgebra, Random
 include("../src/NaviSimu_adder.jl")
 using Main.NaviSimu
 
-function pointPosition(ranges, navSats; niter = 10)
-   esti = [0, 0, 0, 0]
-   for i in 1:niter
-      esti = pointPositionIteration(esti, ranges, navSats)
-   end
-   return Tuple(esti)
-end
+# include("../src/keplerOrbits.jl")
+# include("../src/bodies.jl")
+# include("../src/naviTools.jl")
+# include("../src/naviSignals.jl")
+# include("../src/io/plotTools.jl")
+# include("../src/io/gpsData.jl")
+
+
 
 function distances(refPoint, pointVector)
    distVec = map(x -> norm(pointVector[x] .- refPoint), 1:length(pointVector))
    return distVec
 end
 
-function kinematicIter_old(aPriEst_c, rangeData, phaseData)
+function kinematicIter(aPriEst_c, rangeData, phaseData, availability)
 
-   epochs = size(phaseLens, 1)
-   sats = size(phaseLens, 2)
-   global designMat = hcat(zeros(epochs * sats, epochs * 4), -ones(epochs*sats, sats))
+   avail = availability
+   n_epochs = size(avail, 1)
+   n_sats = size(avail, 2)
+   n_measurements = sum(avail) *2
+   prns = collect(1:size(avail)[2])
 
-   for e in iter
-      designMat[(e-1)*32+1:e*32, (e-1)*4+1:e*4] = NaviSimu.findPPDesignMatrix(Tuple(aPriEst_c[4*e-3:4*e-1]), gpsPositionData(e))
-   end
-
-   global modelRange = []
-   for e in 1:epochs
-      for isat in 1:sats
-         recPos = aPriEst_c[4*e-3:4*e-1]
-         traPos = gpsPositionData(e)[isat]
-         recClockError = aPriEst_c[4*e]
-         bias = aPriEst_c[4*96+isat]
-         geomrange = norm(recPos .- traPos)
-         pseudorange = geomrange + recClockError - bias
-         append!(modelRange, pseudorange)
+   #bias filter
+   global biasNumber = zeros(Int16, n_epochs, maximum(prns))
+   for epoch in 1:n_epochs
+      for prn in prns[avail[epoch, :]]
+         if epoch > 1 && biasNumber[epoch-1, prn] > 0
+            #Continue using old bias if a number has been assigned in previous epoch
+            biasNumber[epoch, prn] = biasNumber[epoch-1, prn]
+         else
+            #Create new bias Number
+            biasNumber[epoch, prn] = maximum(biasNumber) + 1
+         end
       end
    end
 
-   global curr_error = (reshape(phaseLens', iter[end] * sats) - modelRange)
-   correction = pinv(designMat' * designMat) * designMat' * (reshape(phaseLens', iter[end] * sats) - modelRange)
-   correction = pinv(designMat' * designMat) * designMat' * curr_error
-return correction
+   n_bias = maximum(biasNumber)
+   #global designMat = hcat(zeros(epochs * sats*2, epochs * 4), zeros(epochs*sats*2, nbias))
 
-end
-function kinematicIter(aPriEst_c, rangeData, phaseData)
+   global designMat = zeros(Float64,  n_measurements, n_epochs *4 + n_bias)
+   measurements = zeros(Float64, n_measurements)
+   model = zeros(Float64, n_measurements)
+   weightMatrix = Matrix{Float64}(I, n_measurements, n_measurements)
 
-   epochs = size(phaseLens, 1)
-   sats = size(phaseLens, 2)
-   nbias = sats
-   global designMat = hcat(zeros(epochs * sats*2, epochs * 4), zeros(epochs*sats*2, nbias))
-   measurements = []
-   model = []
-   weightMatrix = zeros(epochs*sats*2, epochs*sats*2)
+   aPriEst = aPriEst_c
+   while length(apriEst) < 4*n_epochs
+      apriEst.append(0.0)
+   end
+   aPrioriLength = 4*n_epochs + n_bias
+   while length(apriEst) < aPrioriLength
+      iAddedBias = length(apriEst) - 4*n_epochs +1
+      dataFilter = biasNumber.==iAddedBias
+      aPriBias = sum((rangeData[dataFilter] - phaseData[dataFilter] )) / sum(dataFilter)
+      append!(aPriEst, aPriBias)
+   end
+
    rowIter = 1
-   aPriBias = aPriEst_c[epochs*4+1:end]
-
    #Loop over all epochs
-   for e in 1:epochs
-      designMat[rowIter:rowIter + 2*sats-1, 4*e] = ones(2*sats)   #1s for time
-      gpspos_e = gpsPositionData(e) #navigation constellation positions
-      apri = aPriEst_c[4*e-3:4*e]   #per-epoch apriori estimation
-      #Loop over measurements / satellites
-      for s in 1:sats
-         navsatpos = gpspos_e[s]
-         posdiff = apri[1:3].- collect(navsatpos)
+   for epoch in 1:n_epochs
+      gpspos_e = gpsPositionData(epoch) #navigation constellation positions
+      apri_e = aPriEst_c[4*epoch-3:4*epoch]   #apriori pos en t estimation for this epoch
+
+      #Loop over available satellites
+      for prn in prns[avail[epoch, :]]
+         navsatpos = gpspos_e[prn]
+         posdiff = apri_e[1:3].- collect(navsatpos)
          r = norm(posdiff) #Geometric range
-         designMat[rowIter, 4*e-3:4*e-1] = posdiff/r
-         #designMat[rowIter, epochs*4+1:end] = -ones(32)
-         designMat[rowIter, epochs*4+s] = -1
-         designMat[rowIter+1, 4*e-3:4*e-1] = posdiff/r
+         global sats_uvec = posdiff/r
 
-         weightMatrix[rowIter, rowIter] = 1e3      #Weight for phase m.
-         weightMatrix[rowIter+1, rowIter+1] = 1    #Weight for code m.
+         #Add phase measurement to design matrix and weight matrix
+         designMat[rowIter, 4*epoch-3:4*epoch] = vcat(sats_uvec, 1.0)
+         designMat[rowIter, n_epochs*4 + biasNumber[epoch, prn]] = -1   #bias
+         weightMatrix[rowIter, rowIter] = 1e3      #Weight for phase measurement
 
-         append!(measurements, [phaseData[e, s], rangeData[e, s]])
-         m_pr = r + apri[4]               #model for pseudo range
-         m_ph = r + apri[4] - aPriBias[s] #model for phase
-         append!(model, [m_ph, m_pr])
+         #Add code measurement to design matrix and weight matrix
+         designMat[rowIter+1, 4*epoch-3:4*epoch] = vcat(sats_uvec, 1.0)
+         weightMatrix[rowIter+1, rowIter+1] = 1    #Weight for code measurement
+
+         measurements[rowIter] = phaseData[epoch, prn]
+         measurements[rowIter+1] = rangeData[epoch, prn]
+
+         model_phase = r + apri_e[4] - aPriEst[n_epochs*4 + biasNumber[epoch, prn]] #model for phase
+         model[rowIter] = model_phase
+         model_pseudorange = r + apri_e[4]               #model for pseudo range
+         model[rowIter+1] = model_pseudorange
 
          rowIter += 2
       end
@@ -90,17 +99,6 @@ function kinematicIter(aPriEst_c, rangeData, phaseData)
    correction = inv(designMat' *weightMatrix* designMat) * designMat' *weightMatrix* (measurements - model)
 return correction
 
-end
-
-function pointPositionIteration(aPriEst, rangeData, navconPos)
-   nNavsat = length(navconPos)
-   aPriPos = Tuple(aPriEst[1:3])
-   aPriRange = map(x -> norm(aPriPos .- navconPos[x])+aPriEst[4], 1:length(navconPos))
-   designMat = NaviSimu.findPPDesignMatrix(aPriPos, navconPos)
-
-   #Least squares estimation of position correction
-   deltaPos = pinv(designMat' * designMat) * designMat' * (rangeData - aPriRange)
-   return aPriEst .+ deltaPos
 end
 
 #Example orbit / data from mission geometfry and orbit design
@@ -119,20 +117,24 @@ end
 gpsPositionData(i) = [(gpsdata.x[i, prn], gpsdata.y[i, prn], gpsdata.z[i, prn]) for prn in 1:32]
 gps1 = gpsPositionData(1)
 
+nepochs = 96
+nsats = 32
+codeSig = Array{Float64}(undef, nepochs, nsats)
+phaseSig = Array{Float64}(undef, nepochs, nsats)
+availability = BitArray(undef, nepochs, nsats)
 
-codeSig = reshape([], 0, 32)
-phaseSig = reshape([], 0, 32)
-iter = 1:96
-timevec = (iter.-1) * 15 * 60
+timevec = ((1:nepochs).-1) * 15 * 60
 trueIss = [position(propagateKeplerOrbit(iss, t)) for t in timevec]
+pdop = [findNavPDOP(trueIss[epoch], gpsPositionData(epoch)) for epoch in 1:nepochs]
 
-for ti in iter
-   gpsPos = gpsPositionData(ti)
+for epoch in 1:nepochs
+   gpsPos = gpsPositionData(epoch)        #Constellation positions
+   curSignals = instaSignal(trueIss[epoch], gpsPos, timevec[epoch])
 
-   curSignals = instaSignal(trueIss[ti], gpsPos, timevec[ti])
-   global codeSig = vcat(codeSig, curSignals.code')
-   global phaseSig = vcat(phaseSig, curSignals.phase')
-
+   #Store data
+   codeSig[epoch, :] = curSignals.code
+   phaseSig[epoch, :] = curSignals.phase
+   availability[epoch, :] = curSignals.avail
 end
 
 Random.seed!(1)
@@ -141,17 +143,17 @@ Random.seed!(1)
 freq = 1575.42e6
 waveLen = lightConst / freq
 pseudoRanges = codeSig * lightConst
-pseudoRanges += randn(size(pseudoRanges)) * 1   #1m normal errors
+pseudoRanges += randn(size(pseudoRanges)) .* (pseudoRanges.!=0.0) * 1   #1m normal errors
 phaseLens = phaseSig * waveLen
-phaseLens += randn(size(phaseLens)) *1e-2       #1mm normal errors
+phaseLens += randn(size(phaseLens)) .* (phaseLens.!=0.0) *1e-3       #1mm normal errors
 
 #Perform point position estimation and compare position error (magnitude seems to be 1e-9)
-ppesti = [pointPosition(pseudoRanges[i,:], gpsPositionData(i); niter=5) for i in iter]
-ppPosErrors = [norm(trueIss[i] .- ppesti[i][1:3]) for i in iter]
-
+ppesti = [pointPosition(pseudoRanges[epoch, availability[epoch, :]],
+            gpsPositionData(epoch)[availability[epoch, :]]; niter=5) for epoch in 1:nepochs]
+ppPosErrors = [norm(trueIss[i] .- ppesti[i][1:3]) for i in 1:nepochs]
 
 #Perform kinematic position estimation
-apriPosTime = reshape(hcat(map(x-> collect(ppesti[x]), iter)...), 4*96, 1)
+apriPosTime = vcat(map(x-> collect(ppesti[x]), 1:nepochs)...)
 apriBias = (pseudoRanges[1,:] - phaseLens[1,:])
 apriEst = vcat(apriPosTime, apriBias)
 
@@ -160,11 +162,11 @@ corrections = []
 
 @progress for i in 1:3
       #weights, singulariteit
-   corr = kinematicIter(kinEstim, pseudoRanges,  phaseLens)
+   corr = kinematicIter(kinEstim, pseudoRanges,  phaseLens, availability)
    append!(corrections, norm(corr))
    global kinEstim += corr
 end
-kinPosErrors = [norm(trueIss[i] .- kinEstim[4*i-3:4*i-1]) for i in iter]
+kinPosErrors = [norm(trueIss[i] .- kinEstim[4*i-3:4*i-1]) for i in 1:nepochs]
 
 
 acc1 = sum(ppPosErrors) / length(ppPosErrors)      #pdop * normal error
--- src/naviSignals.jl
+++ src/naviSignals.jl
@@ -18,34 +18,40 @@ end
 
 
 function instaSignal(recPos::Tup3d, transPos::Tup3d, time::Number)
-    #Neglect doppler shifting and relativistic effects
-    freq = 1575.42e6
-    waveLen = lightConst / freq
-    distance = norm(recPos .- transPos)
-    signalTravelTime = distance / lightConst
+    los = hasLineOfSightEarth(recPos, transPos)
+    if los
+        #Neglect doppler shifting and relativistic effects
+        freq = 1575.42e6
+        waveLen = lightConst / freq
+        distance = norm(recPos .- transPos)
+        signalTravelTime = distance / lightConst
 
-    #Neglect clock errors on receiver and transmitter satellite
+        #Neglect clock errors on receiver and transmitter satellite
 
-    #phase at t0 is 0 for receiver and transmitter
-    phaseRe0 = 0
-    phaseTr0 = 1e7
-    phaseRe = phaseRe0 + freq * time                       #+ clock error stuff
-    phaseTr = phaseTr0 + freq * (time - signalTravelTime) #+ clock error stuff
+        #phase at t0 is 0 for receiver and transmitter
+        phaseRe0 = 0
+        phaseTr0 = 1e7
+        phaseRe = phaseRe0 + freq * time                       #+ clock error stuff
+        phaseTr = phaseTr0 + freq * (time - signalTravelTime) #+ clock error stuff
 
-    phase = phaseRe - phaseTr #+ N + error
-
-    return (code = signalTravelTime, phase = phase)
+        phase = phaseRe - phaseTr #+ N + error
+    else
+        signalTravelTime = 0.0
+        phase = 0.0
+    end
+    return (code = signalTravelTime, phase = phase, los = los)
 end
 
 function instaSignal(recPos::Tup3d, transPos::Array{Tup3d}, time::Number)
     nsats = length(transPos)
-    codes = zeros(nsats)
-    phases = zeros(nsats)
-
+    codes = Array{Float64, 1}(undef, length(transPos))
+    phases = Array{Float64, 1}(undef, length(transPos))
+    avail = BitArray(undef, length(transPos))
     for isat in 1:nsats
         res = instaSignal(recPos, transPos[isat], time)
         codes[isat] = res.code
         phases[isat] = res.phase
+        avail[isat] = res.los
     end
-    return (code = codes, phase = phases)
+    return (code = codes, phase = phases, avail = avail)
 end
