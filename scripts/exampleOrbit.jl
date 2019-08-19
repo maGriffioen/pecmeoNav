199bd48af22b6b003095dc2992a4261c06622a01
Mischa <m.a.griffioen@student.tudelft.nl>
2019-08-19 10:56:08 +0200

    Added precise orbits

--- scripts/exampleOrbit.jl
+++ scripts/exampleOrbit.jl
@@ -1,4 +1,4 @@
-using Plots, ProgressMeter, LinearAlgebra, Random#, DoubleFloats
+using Plots, ProgressMeter, LinearAlgebra, Random, DoubleFloats
 include("../src/NaviSimu_lessModule.jl")
 # using Main.NaviSimu
 
@@ -19,11 +19,17 @@ iss = KeplerOrbit(6787746.891, 0.000731104,
     deg2rad(74.21987137), deg2rad(24.10027677), earth)
 moonSat = KeplerOrbit(moon.radius + 100e3, 0.0,
     deg2rad(0), 0.0, 0.0, 0.0, moon)
+moonSat_p = KeplerOrbit(moon.radius + df64"100e3", df64"0.0", df64"0.0", df64"0.0", df64"0.0", df64"0.0", moon)
+KeplerOrbit(1.8381e6, 0.0, 0.0, 0.0, 0.0, 0.0, Body("Moon", 4.9027204e12, 1.7381e6, moonState))
 #Pecmeo after optimization run
 pecmeo_333 = createCircPecmeo(26.4e6, (3, 3, 3), earth;
     initialOrbitShift=(0.0, (2/6)*pi, (2/6)*pi),
     equatorialRotation = 0*pi/8,
     inclination = 0.0)
+pecmeo_333_p = createCircPecmeo(df64"26.4e6", (3, 3, 3), earth;
+    initialOrbitShift=(df64"0.0", 2 * Double64(pi) /6, 2 * Double64(pi) /6),
+    equatorialRotation = df64"0.0",
+    inclination = df64"0.0")
 #Pecmeo of 3x2 satellites
 pecmeo_222 = createCircPecmeo(26.4e6, (2, 2, 2), earth,
     (2.5891201128936614, 2.496350075955589, 0.6181176114714648);
@@ -40,11 +46,11 @@ pecmeo_222 = createCircPecmeo(26.4e6, (2, 2, 2), earth,
 # gps1 = gpsPositionData(1)
 
 ### Set up epochs and time vector ###
-nepochs = 100   #Number of time steps
+nepochs = 2880      #Number of time steps
 timestep = 30    #seconds
 t0 = 0
 timevec = t0 .+ ((1:nepochs).-1) * timestep
-
+timevec_org = collect(timevec)
 ### Set up constellation and measurement storage ###
 navcon = pecmeo_333
 receiverOrbit = moonSat
@@ -62,13 +68,20 @@ availability = BitArray(undef, nepochs, nsats)     #Per-epoch, per-prn availabil
 
 ### Real satellite positon ###
 truePositions = [globalPosition(receiverOrbit, t) for t in timevec]
+truePositions_p = [globalPosition(moonSat_p, Double64(t)) for t in timevec]
+truePosError = [norm(truePositions[i] .- truePositions_p[i]) for i in 1:length(truePositions)]
 pdop = [findNavPDOP(truePositions[epoch], globalPosition(navcon, timevec[epoch])) for epoch in 1:nepochs]
 gdop = [findNavGDOP(truePositions[epoch], globalPosition(navcon, timevec[epoch])) for epoch in 1:nepochs]
 
-
+conPosErrors = []
+conPosErrorRSS = []
 ### Generation of measurements ###
 for epoch in 1:nepochs
    conPos = globalPosition(navcon, timevec[epoch])         #Constellation positions
+   conPos_p = globalPosition(pecmeo_333_p, Double64(timevec[epoch]))
+   append!(conPosErrors, [norm(conPos[i] .- conPos_p[i]) for i in 1:length(conPos)])
+   append!(conPosErrorRSS, norm([norm(conPos[i] .- conPos_p[i]) for i in 1:length(conPos)]))
+
    curSignals = instaSignal(truePositions[epoch], conPos, timevec[epoch])
 
    #Store data
@@ -76,6 +89,7 @@ for epoch in 1:nepochs
    phaseSig[epoch, :] = curSignals.phase
    availability[epoch, :] = curSignals.avail
 end
+conPosErrors = reshape(conPosErrors, (size(navcon), nepochs))
 
 # Remove epochs with too few satellites --- TODO: Improve
 satCount = [sum(availability[e, :]) for e in 1:size(availability)[1]]
@@ -96,10 +110,9 @@ Random.seed!(1)
 freq = 1575.42e6
 waveLen = lightConst / freq
 pseudoRanges = codeSig * lightConst
-# pseudoRanges += randn(size(pseudoRanges)) .* (pseudoRanges.!=0.0) * 1 *0   #1m normal errors
-# pseudoRanges += randn(size(pseudoRanges)) .* (pseudoRanges.!=0.0) * 2   #1m normal errors
+pseudoRanges += randn(size(pseudoRanges)) .* (pseudoRanges.!=0.0) * 1   #1m normal errors
 phaseLens = phaseSig * waveLen
-# phaseLens += randn(size(phaseLens)) .* (phaseLens.!=0.0) *1e-3 *0       #1mm normal errors
+phaseLens += randn(size(phaseLens)) .* (phaseLens.!=0.0) *1e-3        #1mm normal errors
 
 ### Point position estimation ###
 ppApriori = vcat([vcat([j for j in bodyPosition(moon, timevec[i])], 0.0)' for i in 1:nepochs]...)
@@ -109,7 +122,8 @@ ppPosErrors = [norm(truePositions[i] .- ppesti[i][1:3]) for i in 1:nepochs]
 
 ### Kinematic estimation
 @time kinEstim = kinematicEstimation(navconEphemeres, timevec, pseudoRanges, phaseLens, availability;
-    ppApriori = ppApriori, maxIter_pp=100, maxIter_kin = 5, codeWeight = 1.0, phaseWeight = 1e6)
+    ppApriori = ppApriori, maxIter_pp=100, maxIter_kin = 5, codeWeight = 1.0, phaseWeight = 1.0e6,
+    lighttimeCorrection = false, solver = 2)
 kinPosTime = kinEstim.positionTimeEstimation
 kinBias = kinEstim.biasEstimation
 
@@ -121,3 +135,10 @@ acc1 = sum(ppPosErrors) / length(ppPosErrors)      #pdop * normal error
 acc2 = sum(kinPosErrors) / length(kinPosErrors)
 print("\n PointPosi error: \t", acc1)
 print("\n Kinematic error: \t", acc2)
+
+
+# scatter!(timevec/3600, kinPosErrors, marker=(:dot, 3), lab="Navigation Error, no time dependent error")
+# # plot!(timevec/3600, pdop/1000000, lab="PDOP/1000")
+# xaxis!("Time since epoch [h]")
+# yaxis!("Navigation error [m] AND pdop/1E7 [-]")
+# plot!(legend=:topleft)
